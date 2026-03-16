d82530d766cd83580dbd4dbe843a7247f75af621
Amirlabai <199509007+Amirlabai@users.noreply.github.com>
2026-03-17 00:12:30 +0200

    playoff

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -40,7 +40,7 @@ function App() {
               <img src="/to-be-logo.svg" className="header-side-logo left" alt="" />
               <img src="/Flag_of_Adygea.svg" className="header-side-logo right" alt="" />
               <h1 className="display-4 fw-bold">טורניר נצ'מאז<br />1447/2026</h1>
-              <p className="lead" id="tournamentPhase">שלב הבתים</p>
+              <p className="lead" id="tournamentPhase">פלייאוף</p>
             </header>
           </div>
 
@@ -72,7 +72,7 @@ function App() {
 
         {/* Footer */}
         <Footer />
-        
+
         <ScrollToTop />
 
         <Analytics />
